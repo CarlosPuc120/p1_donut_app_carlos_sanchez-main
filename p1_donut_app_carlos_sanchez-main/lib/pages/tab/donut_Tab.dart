47fb68e5d3ef0a8a2b31714e75a4e9655cbabf52
Carlos Puc <puucarlos672@gmail.com>
2024-10-17 10:56:33 -0600

    donutTie added

--- p1_donut_app_carlos_sanchez-main/lib/pages/tab/donut_Tab.dart
+++ p1_donut_app_carlos_sanchez-main/lib/pages/tab/donut_Tab.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:p1_donut_app_carlos_sanchez/Utilis/donut_tile.dart';
 
 class DonutTab extends StatelessWidget {
+  // list of donuts
+  final List donutsOnSale = const[ 
+  //[ donutFlavor, donutPrice, donutColor, imageName ]
+  ["Ice Cream", "36", Colors.blue, "lib/images/icecream_donut.png"],
+  ["Strawberry", "45", Colors.red, "lib/images/strawberry_donut.png"],
+  ["Grape Ape", "84", Colors.purple, "lib/images/grape_donut.png"],
+  ["Choco", "95", Colors.brown, "lib/images/chocolate_donut.png"],]; 
   const DonutTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  const Center(child: Text("DonutTab"),);
+    return  GridView.builder(gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 1/1.5), 
+   itemCount: donutsOnSale.length,
+    itemBuilder: (context, index ) { 
+      return DonutTile(
+        donutFlavor: donutsOnSale[index][0],
+        donutPrice: donutsOnSale[index][1],
+        donutColor: donutsOnSale[index][2],
+        imageName: donutsOnSale[index][3],
+      );
+
+    }) ;
   }
 }
